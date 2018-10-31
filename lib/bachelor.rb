bddd52330652c2980c71e0bb8a600f03d3baf459
Damon Collins <damon.c.p.collins@gmail.com>
2018-10-31 05:40:54 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,6 @@
 def get_first_name_of_season_winner(data, season)
   # code here
-  data[season]["status"] == "winner"
+  data[season]["status"] == "Winner"
 end
 
 def get_contestant_name(data, occupation)
